572c283652ddb1e9241621c8b41d28dd3833dac1
MaksymKolii <maksymkolii@gmail.com>
2023-01-12 00:01:12 +0100

    commit

--- src/redux/contacts/contacts-operations.js
+++ src/redux/contacts/contacts-operations.js
@@ -2,3 +2,16 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 axios.defaults.baseURL ='https://63befa7b585bedcb36bc5122.mockapi.io/contacts'
+
+export const fetchContacts = createAsyncThunk('contacts/fetchAll', async (_,{rejectWithValue})=>
+{try {
+  const {data}=  await axios.get()
+  console.log(data);
+  return data
+    
+} catch (error) {
+    return rejectWithValue(error)
+    
+}})
+export const addContact  = createAsyncThunk('contacts/addContact')
+export const deleteContact = createAsyncThunk('contacts/deleteContact')
