74c43078fbc326c1e26bd1f77cec47ca8ac5d2ef
Maksym Kolii <maksymkolii@gmail.com>
2023-01-13 15:22:18 +0100

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,40 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { Button } from "./Button/Button";
-import { ContactForm } from "./ContactForm/ContactForm";
-import { fetchContacts } from "redux/contacts/contacts-operations";
-import { ContactsList } from "./ContactsList/ContactsList";
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Button } from './Button/Button';
+import { ContactForm } from './ContactForm/ContactForm';
+import { fetchContacts } from 'redux/contacts/contacts-operations';
+import { ContactsList } from './ContactsList/ContactsList';
 
 export const App = () => {
+  const [isListShown, setIsListShown] = useState(false);
+  const [isFormShown, setFormShown] = useState(false);
+  const dispatch = useDispatch();
 
-  const [isListShown, setIsListShown] = useState(false)
-  const [isFormShown, setFormShown] = useState(false)
-  const dispatch = useDispatch()
+  const showContactsList = () => {
+    setIsListShown(true);
+    dispatch(fetchContacts());
+  };
 
-  const showContactsList = ()=>{
-    setIsListShown(true)
-    dispatch(fetchContacts())
-  }
-
-  const showForm =()=>{
-    setFormShown(true)
-  }
+  const showForm = () => {
+    setFormShown(true);
+  };
   return (
     <>
-    {isFormShown && <ContactForm/>}
-    
-    <Button text='Show form' clickHandler ={showForm}></Button> 
-      
-      {isListShown ? <ContactsList/> : <Button text='Show contacts' clickHandler ={showContactsList}></Button> }
+      {/* {isFormShown && <ContactForm />} */}
+      {/* <Button text="Add contact" clickHandler={showForm}></Button> */}
+
+      {isListShown ? (
+        <>
+          {' '}
+          {!isFormShown && (
+            <Button text="Add contact" clickHandler={showForm}></Button>
+          )}
+          {isFormShown && <ContactForm />}
+          <ContactsList />
+        </>
+      ) : (
+        <Button text="Show contacts" clickHandler={showContactsList}></Button>
+      )}
     </>
   );
 };
