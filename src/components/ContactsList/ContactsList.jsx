51cd1c7b886ff411c174349a9300b7ef43ab6cad
Maksym Kolii <maksymkolii@gmail.com>
2023-01-16 18:18:38 +0100

    commit

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,4 +1,4 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector} from 'react-redux';
 import { ContactListItem } from '../ContactItem/ContactItem';
 import { ListUl } from './ContactList.styled';
 import { Button } from '../Button/Button.styled';
